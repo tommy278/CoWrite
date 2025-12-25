7230f1a94298bc82c87ced8c400b96b3e1501dbd
Tommy <SUP3RS4IY4NX@gmail.com>
2025-12-24 21:16:22 -0500

    made production changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import viteReact from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro(),
     viteReact(),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
